01cde11ec73e71a1d4590001414b423a99dbe70d
Wilian Hiroyuki Morimoto <158129043+wilianmorimoto@users.noreply.github.com>
2024-05-25 12:42:48 -0300

    initial commit

---
+++ app/components/Button.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface ButtonProps {
+	type: "button" | "submit";
+	title: string;
+	icon?: string;
+	variant: string;
+	full?: boolean;
+}
+
+export default function Button({
+	type,
+	title,
+	icon,
+	variant,
+	full,
+}: ButtonProps) {
+	return (
+		<button
+			type={type}
+			className={`flexCenter gap-3 rounded-full border ${variant} ${
+				full && "w-full"
+			}`}
+		>
+			{icon && <Image src={icon} alt={title} width={24} height={24} />}
+			<label className="bold-16 whitespace-nowrap pointer-events-none">
+				{title}
+			</label>
+		</button>
+	);
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Button from "./Button";
+
+const Navbar = () => {
+	const [showMenu, setShowMenu] = useState(false);
+
+	return (
+		<nav className="flexBetween max-container padding-container relative z-30 py-5">
+			<Link href="/">
+				<Image
+					src="/hilink-logo.svg"
+					width={74}
+					height={29}
+					alt="Logo Hilink"
+				/>
+			</Link>
+			<ul className="hidden h-full gap-12 lg:flex">
+				{NAV_LINKS.map((link) => (
+					<Link
+						key={link.key}
+						href={link.href}
+						className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+					>
+						{link.label}
+					</Link>
+				))}
+			</ul>
+			<div className="lg:flexCenter hidden">
+				<Button
+					type="button"
+					title="Login"
+					icon="/user.svg"
+					variant="btn_dark_green"
+				/>
+			</div>
+
+			<Image
+				src="menu.svg"
+				alt="menu"
+				width={32}
+				height={32}
+				className="inline-block cursor-pointer lg:hidden z-50"
+				onClick={() => setShowMenu(!showMenu)}
+			/>
+			<ul className={`absolute right-0 top-0 z-40 shadow-xl h-screen px-8 pt-14 w-1/3 flex flex-col gap-4 duration-300 bg-white lg:hidden ${!showMenu && 'hidden'}`}>
+				{NAV_LINKS.map((link) => (
+					<Link
+						key={link.key}
+						href={link.href}
+						className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            onClick={() => setShowMenu(false)}
+					>
+						{link.label}
+					</Link>
+				))}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
